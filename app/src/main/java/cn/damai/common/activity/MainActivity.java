4c7e9ab9702053381374005c9667d9028cfd0b8a
wanggeng <mtwanggeng@damai.cn>
2017-05-08 10:04:53 +0800

    init commit

---
+++ app/src/main/java/cn/damai/common/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.damai.common.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.damai.common.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
